2c81f1fb2b4e32ce2d9df9241f895063f31a6e15
cfrost <cassandra.e.hutchings@gmail.com>
2023-04-06 13:01:23 -0600

    new version

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import {React, useEffect, useState} from "react";
+import { fetchAllPuppies } from "../API/FetchPuppies";
+import "../App.css"
+
+const Home = () => {
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        const getAllPuppies = async () => {
+            const players = await fetchAllPuppies();
+            setPlayers(players);
+        }
+        console.log(getAllPuppies);
+    })
+
+}
